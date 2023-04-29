ce16eb9825cbd85ed20d16b2c5ee1bf4ed42eeb4
britneyfisher <76415339+britneyfisher@users.noreply.github.com>
2023-04-29 10:09:09 -0400

    idk...

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-//alert("hello world! ");
+let apiKey = "3e320afcoa25c0e8104373fa1a244bte";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?Lisbon={query}&key={apiKey}";
+
+console.log(apiUrl);
