54067e4d7233e4815b6a259f7d5911536e2b02cb
britneyfisher <76415339+britneyfisher@users.noreply.github.com>
2023-04-30 08:55:25 -0400

    updated API key

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-let apiKey = "3e320afcoa25c0e8104373fa1a244bte";
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?Lisbon={query}&key={apiKey}";
+  "https://api.shecodes.io/weather/v1/current?Lisbon={query}&key={apiKey}&units=metric";
 
 console.log(apiUrl);
+
+//axios.get(apiUrl).then(displayTemperature);
