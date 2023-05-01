d380a13245f1ff12d0386d9a22ac7f33b74324ae
britneyfisher <76415339+britneyfisher@users.noreply.github.com>
2023-04-30 22:09:45 -0400

    apiKey fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 function displayTemperature(response) {
   console.log(response.data);
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?Lisbon={query}&key={apiKey}&units=metric";
+let query = "London";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
 
-console.log(apiUrl);
+//console.log(apiUrl);
 
-//axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
