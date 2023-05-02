d8c25bf1b8e525cfee185f8af2605d34d82ecb4f
britneyfisher <76415339+britneyfisher@users.noreply.github.com>
2023-05-02 06:20:48 -0400

    updated structure

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.condition.humidity;
+  wind.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
